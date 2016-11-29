6bb2c9da28a117db19bf7b0a362dc684d6f219c0
ohalloranlab <dohalloran@users.noreply.github.com>
2016-11-29 11:34:25 -0500

    Update PrimerMap.pm

--- lib/PrimerMap.pm
+++ lib/PrimerMap.pm
@@ -18,72 +18,70 @@ PrimerMap - generates a primer sequence graphcial map
 
 =head1 SYNOPSIS
 
-use PrimerMap;
-use Data::Dumper;
-
-my $start  = "12,24,28,32,824,888,902";
-my $end    = "40,59,48,50,801,848,880";
-my $output = "myGene.png";
-
-# can also collect primer coordinates from a text file 
-# my $inFile = "primermap_file.txt";
-# $tmp->read_file($inFile);
-
-my $tmp = PrimerMap->new();
-
-$tmp->load_map(
+ use PrimerMap;
+ use Data::Dumper;
+ 
+ my $start  = "12,24,28,32,824,888,902";
+ my $end    = "40,59,48,50,801,848,880";
+ my $output = "myGene.png";
+ 
+ # can also collect primer coordinates from a text file 
+ # my $inFile = "primermap_file.txt";
+ # $tmp->read_file($inFile);
+ 
+ my $tmp = PrimerMap->new();
+ $tmp->load_map(
     primer_start => $start,
     primer_end   => $end,
     seq_length   => "1200",
     gene_name    => "myGene",
     out_file     => $output || "output.png"
-);
+ );
 
-# take a look at the object 
-print Dumper($tmp);
+ # take a look at the object 
+ print Dumper($tmp);
 
-# print the primer map
-$tmp->primer_map();
-print "\n\n";
+ # print the primer map
+ $tmp->primer_map();
+ print "\n\n";
 
-# getters and setters can be used as follows:
-my $set_start = $tmp->set_start("22,24,2226");
-print $set_start. "\n\n";
+ # getters and setters can be used as follows:
+ my $set_start = $tmp->set_start("22,24,2226");
+ print $set_start. "\n\n";
 
-my $get_start = $tmp->get_start();
-print $get_start. "\n\n";
+ my $get_start = $tmp->get_start();
+ print $get_start. "\n\n";
 
-my $set_end = $tmp->set_end("52,64,2202");
-print $set_end. "\n\n";
+ my $set_end = $tmp->set_end("52,64,2202");
+ print $set_end. "\n\n";
 
-my $get_end = $tmp->get_end();
-print $get_end. "\n\n";
+ my $get_end = $tmp->get_end();
+ print $get_end. "\n\n";
 
-my $set_length = $tmp->set_length(2500);
-print $set_length. "\n\n";
+ my $set_length = $tmp->set_length(2500);
+ print $set_length. "\n\n";
 
-my $get_length = $tmp->get_length();
-print $get_length. "\n\n";
+ my $get_length = $tmp->get_length();
+ print $get_length. "\n\n";
 
-my $set_id = $tmp->set_ID("myNewGeneName");
-print $set_id. "\n\n";
+ my $set_id = $tmp->set_ID("myNewGeneName");
+ print $set_id. "\n\n";
 
-my $get_id = $tmp->get_ID();
-print $get_id. "\n\n";
+ my $get_id = $tmp->get_ID();
+ print $get_id. "\n\n";
 
-my $set_file = $tmp->set_outfile("myOutPutFile.png");
-print $set_file. "\n\n";
+ my $set_file = $tmp->set_outfile("myOutPutFile.png");
+ print $set_file. "\n\n";
 
-my $get_file = $tmp->get_outfile();
-print $get_file. "\n\n";
+ my $get_file = $tmp->get_outfile();
+ print $get_file. "\n\n";
 
-# see the newly set object
-print Dumper($tmp);
+ # see the newly set object
+ print Dumper($tmp);
 
-# see the newly set primer map
-$tmp->primer_map();
-print "\n\n";
-
+ # see the newly set primer map
+ $tmp->primer_map();
+ print "\n\n";
 
 =head1 DESCRIPTION
 
@@ -107,7 +105,7 @@ Please include a thorough description of the problem with code and data examples
 
 Report bugs to the GitHub bug tracking system to help keep track of the bugs and their resolution.  Bug reports can be submitted via the GitHub page:
 
-https://github.com/dohalloran/Bio-SeqFeature-Generic-Ext-PrimerMap/issues
+ https://github.com/dohalloran/Bio-SeqFeature-Generic-Ext-PrimerMap/issues
   
 =head1 AUTHORS - Damien OHalloran
 
